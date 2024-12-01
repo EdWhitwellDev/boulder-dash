4d037f06e69b7b88e61de4ee7543f684cd93030a
yejunkimm <2330689@swansea.ac.uk>
2024-12-01 21:21:15 +0000

    Update Key.java
    Maybe it is better to have enum as separate class

--- src/main/java/com/example/boulderdash/Tiles/Key.java
+++ src/main/java/com/example/boulderdash/Tiles/Key.java
@@ -3,6 +3,16 @@ package com.example.boulderdash.Tiles;
 import javafx.scene.image.Image;
 
 public class Key extends Floor{
+     private Colour keyColour;
+
+     enum Colour {
+        RED, GREEN, YELLOW, BLUE
+    }
+
+    public Colour getKeyColour() {
+        return keyColour;
+    }
+    
     public Key(int row, int col){
         super(row, col, true);
         image = new Image("red_key.png");
