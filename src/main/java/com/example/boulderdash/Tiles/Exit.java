4fe0685ae9cdd0c43d9ca3b310b312421d2640a9
yejunkimm <2330689@swansea.ac.uk>
2024-12-01 21:18:24 +0000

    Update Exit.java
    I have tried to put all of my codes

--- src/main/java/com/example/boulderdash/Tiles/Exit.java
+++ src/main/java/com/example/boulderdash/Tiles/Exit.java
@@ -3,8 +3,23 @@ package com.example.boulderdash.Tiles;
 import javafx.scene.image.Image;
 
 public class Exit extends Floor{
+    //example diamonds needed
+    private static final int DIAMONDS_NEEDED = 10;
+    
     public Exit(int row, int col){
         super(row, col, false);
         image = new Image("exit.png");
     }
+
+      public void exitLevel(Player player, Level level) {
+        if (player.getCollectedDiamonds() >= DIAMONDS_NEEDED) {
+            level.updateLevelStatus(this);
+        } else {
+            System.out.println("You need more diamonds to exit!");
+        }
+
+    public Exit() {
+        this.isExit = true;
+    }
+    
 }
