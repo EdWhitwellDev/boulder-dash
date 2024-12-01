db19f11291006d39ca8f3f6ca353ed2be471d0fd
yejunkimm <2330689@swansea.ac.uk>
2024-12-01 21:23:09 +0000

    Update LockedDoor.java

--- src/main/java/com/example/boulderdash/Tiles/LockedDoor.java
+++ src/main/java/com/example/boulderdash/Tiles/LockedDoor.java
@@ -7,4 +7,24 @@ public class LockedDoor extends Tile{
         super(row, col, false)
 ;        image = new Image("red_door.png");
     }
+
+   public void unlockDoor(Key key) {
+        if ( key.getColour() == this.colour) {
+            isLocked = false;
+            System.out.println("Door unlocked successfully!");
+        } else if (!isLocked) {
+            System.out.println("The door is already unlocked.");
+        } else {
+            System.out.println("Key color does not match the door's color.");
+        }
+    }
+
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public Colour getColour() {
+        return colour;
+    }
 }
