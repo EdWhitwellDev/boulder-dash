e1ff7c2bfbda26378675eb0a55ec2e4a2718c0b9
dylancourt <court.dlc20@gmail.com>
2024-12-02 18:39:35 +0000

    LoseGame and winGame methods modified

--- src/main/java/com/example/boulderdash/Tiles/Exit.java
+++ src/main/java/com/example/boulderdash/Tiles/Exit.java
@@ -1,25 +1,27 @@
 package com.example.boulderdash.Tiles;
 
+import com.example.boulderdash.Actors.Player;
+import com.example.boulderdash.GameManager;
+import com.example.boulderdash.GameState;
+import com.example.boulderdash.Level;
 import javafx.scene.image.Image;
 
-public class Exit extends Floor{
+public class Exit extends Floor {
     //example diamonds needed
     private static final int DIAMONDS_NEEDED = 10;
-    
-    public Exit(int row, int col){
+
+    public Exit(int row, int col) {
         super(row, col, false);
         image = new Image("exit.png");
     }
 
-      public void exitLevel(Player player, Level level) {
+    public void exitLevel(Player player, Level level) {
         if (player.getCollectedDiamonds() >= DIAMONDS_NEEDED) {
             level.updateLevelStatus(this);
+            GameState.manager.winGame();
         } else {
             System.out.println("You need more diamonds to exit!");
         }
 
-    public Exit() {
-        this.isExit = true;
     }
-    
 }
