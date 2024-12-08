bb16aa629308a0981fb176415a4d9646ee728fcc
Viraj Shah <virajashah@aol.com>
2024-12-08 14:46:46 +0000

    Javadoc

--- src/main/java/com/example/boulderdash/Audio.java
+++ src/main/java/com/example/boulderdash/Audio.java
@@ -3,15 +3,27 @@ package com.example.boulderdash;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
-
+/**
+ * Handles the playback of music and sound effects during the game.
+ * @author Viraj Shah
+ * @version 1.0
+ */
 public class Audio {
     private static Audio instance;
     private MediaPlayer mediaPlayer;
 
-    // Private constructor to enforce singleton
+    /**
+     * Private constructor for single patterns.
+     * Initialises audio only once.
+     */
     private Audio() {
     }
-    // Get the single instance of MusicManager
+
+    /**
+     * Returns the single instance of the Audio class.
+     * Creates one if it does not exist.
+     * @return the single instance.
+     */
     public static Audio getInstance() {
         if (instance == null) {
             instance = new Audio();
@@ -19,6 +31,12 @@ public class Audio {
         return instance;
     }
 
+    /**
+     * Plays background music.
+     * @param filePath is where the music is located.
+     * @param loop {@code True} to loop the music.
+     * @param volume the volume of the music (0.0 to 1.0).
+     */
     public void playMusic(String filePath, boolean loop, double volume) {
         try {
             String music = getClass().getResource(filePath).toExternalForm();
@@ -42,6 +60,11 @@ public class Audio {
         }
     }
 
+    /**
+     * Plays a sound effect once.
+     * @param filePath is where the music is located.
+     * @param volume the volume of the music (0.0 to 1.0).
+     */
     public void playSoundEffect(String filePath, double volume) {
         try {
             String sound = getClass().getResource(filePath).toExternalForm();
